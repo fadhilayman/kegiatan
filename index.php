7ffd9e994a72c20e7cb63795f8c2903e296f8d1c
fadhil <aymanfadhil029@gmail.com>
2024-04-29 14:50:48 +0700

    rename

--- index.php
+++ index.php
@@ -1,96 +1,95 @@
-    
-
- <!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <!-- bootstrap cdn  -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-            integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-        <!-- fullcalendar css  -->
-        <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/main.css' rel='stylesheet' />
-    </head>
-    
-    <body>
-        <div class="container mt-4">
-            <div class="row">
-                <div class="col-lg-4">
-                    <div class="alert alert-warning" role="alert">
-                        <h4>Form Kegiatan</h4>
-                    </div>
-                    <div class="card">
-                        <form action="simpan.php" method="POST">
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <div class="form-label">Keterangan Kegiatan</div>
-                                    <textarea name="kegiatan" class="form-control" id="kegiatan" cols="30"
-                                        rows="2"></textarea>
-                                </div>
-                                <div class="form-group">
-                                    <div class="form-label">Ruangan</div>
-                                    <input type="text" class="form-control" name="ruangan" id="ruangan">
-                                </div>
-                                <div class="form-group mt-4">
-                                    <div class="form-label">Tgl Mulai</div>
-                                    <input type="datetime-local" class="form-control" name="mulai" id="mulai">
-                                </div>
-                                <div class="form-group mt-4">
-                                    <div class="form-label">Tgl Selesai</div>
-                                    <input type="datetime-local" class="form-control" name="selesai" id="selesai">
-                                </div>
-                                <div class="form-group">
-                                    <div class="form-label">Keterangan</div>
-                                    <input type="text" class="form-control" name="ket" id="ket">
-                                </div>
-                                <div class="form-group mt-4">
-                                    <button type="submit" class="btn btn-success">Simpan</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="col-lg-8">
-                    <div id="calendar"></div>
-                </div>
-            </div>
-        </div>
-        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.8.0/main.js'></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"
-            integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ=="
-            crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script>
-            document.addEventListener('DOMContentLoaded', function () {
-                var calendarEl = document.getElementById('calendar');
-                var calendar = new FullCalendar.Calendar(calendarEl, {
-                    initialView: 'dayGridMonth',
-                    events: [<?php 
+<?php
     include 'koneksi.php';
-    //melakukan koneksi ke database
-    // $koneksi    = mysqli_connect('localhost', 'root', '', 'latihan');
-    //mengambil data dari tabel jadwal
-    $data       = mysqli_query($conn,'select * from tb_jadwal');
-    //melakukan looping
-    while($d = mysqli_fetch_array($data)){     
-?>
-{
-    title: '<?php echo $d['kegiatan']; ?>', //menampilkan title dari tabel
-    start: '<?php echo $d['mulai']; ?>', //menampilkan tgl mulai dari tabel
-    end: '<?php echo $d['selesai']; ?>' //menampilkan tgl selesai dari tabel
-},
-<?php } ?>],
-                    selectOverlap: function (event) {
-                        return event.rendering === 'background';
+    session_start();
+
+    if (isset($_SESSION['login'])) {
+      if($_SESSION['role'] == "1"){
+        header("Location: kegiatan.php");
+        exit();
+      }elseif($_SESSION['role'] == "2"){
+        header("Location: kegiatan_admin.php?id=$_SESSION[id_user]");
+        exit();
+    }
+  
+    if(isset($_POST['submit'])){
+        $uname = $_POST['uname'];
+        $psw = $_POST['psw'];
+
+        $result = mysqli_query($Conn, "SELECT * FROM tb_user WHERE username='$uname'");
+
+        if(mysqli_num_rows($result) === 1){
+            $row = mysqli_fetch_assoc($result);
+            if(password_verify($psw, $row['password'])){
+                if($row['role'] == 1){
+
+                            $_SESSION["login"] = true;
+                            $_SESSION["role"] = $row['role'];
+
+                            echo "<script>
+                                    alert('Login Berhasil');
+                                    window.location.href='kegiatan.php';
+                                </script>";
+                            exit();
+
+                }elseif($row['role'] == 2){
+
+                            $_SESSION["login"] = true;
+                            $_SESSION["role"] = $row['role'];
+                            $_SESSION["id_user"] = $row['id_user'];
+
+                            echo "<script>
+                                    alert('Login Berhasil');
+                                    window.location.href='kegiatan_admin.php?id=$row[id_user]';
+                                </script>";
+                            exit();
+
+                        }
                     }
-                });
-    
-                calendar.render();
-            });
-        </script>
-    </body>
-    
-    </html>
+                }else{
+                  echo "<script>
+                          alert('Username atau Password salah');
+                          window.location.href='index.php';
+                        </script>";
+                  exit();
+                }
+            }
+        }
+        ?>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Kegiatan SMKN 2 Cimahi</title>
+            <link rel="stylesheet" href="login.css">
+        </head>
+        <body>
+        <center><h2>Login Kegiatan</h2></center>
+        
+<form action="index.php" method="POST">
+  <div class="imgcontainer">
+    <img src="image/logo.JPG" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname">
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw">
+        
+    <button type="submit" name="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+  <p>&copy; 2024 By <b>Fadhil</b> & <b>IM</b></p>
+  </div>
+</form>
+</body>
+</html>
+        
